5b7821dc6cd2ff1d2d6f40d7b2b44e07523105f4
Soren <soren.zemmour@gmail.com>
2025-11-21 21:06:03 +0100

    feat: add trained model.pkl for Render deployment

--- api/model/loader.py
+++ api/model/loader.py
@@ -1,30 +1,13 @@
+import joblib
 import os
-import mlflow
-import mlflow.sklearn
 
-RUN_ID = "220b6b0558b049688b2ece173f794542"
-MODEL_URI = f"runs:/{RUN_ID}/model"
-
+MODEL_PATH = "model/model.pkl"
 model = None
 
 def load_model():
     global model
     if model is None:
-
-        # ---- NEW : mode test → utiliser modèle mock ----
-        if os.environ.get("TESTING") == "1":
-            print("⏳ Mode TEST : chargement du mock model")
-            from api.model.mock_model import MockModel
-            model = MockModel()
-            return model
-
-        # ---- Mode normal : charger le modèle MLflow ----
-        try:
-            print("🔄 Chargement du modèle depuis MLflow...")
-            model = mlflow.sklearn.load_model(MODEL_URI)
-            print("✅ Modèle chargé.")
-        except Exception as e:
-            print(f"❌ Erreur lors du chargement du modèle : {e}")
-            raise RuntimeError("Impossible de charger le modèle depuis MLflow.")
-
+        print("🔄 Chargement du modèle local...")
+        model = joblib.load(MODEL_PATH)
+        print("✅ Modèle chargé.")
     return model
