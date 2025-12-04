3382601c0e9f5dd88c800f38706f38c4a227f22c
Soren <soren.zemmour@gmail.com>
2025-12-04 16:13:36 +0100

    fix: test predict invalid

--- tests/test_predict_invalid.py
+++ tests/test_predict_invalid.py
@@ -4,10 +4,11 @@ from api.main import app
 client = TestClient(app)
 
 def test_predict_invalid_payload():
-    wrong_sample = {
-        "DAYS_BIRTH": -15000  # manque 90% du payload
-    }
+    wrong_sample = {"DAYS_BIRTH": -15000}
 
     response = client.post("/predict", json=wrong_sample)
-    assert response.status_code == 200
+
+    # On attend une erreur 422 car Pydantic devrait rejeter le payload
+    assert response.status_code == 422
+
 
